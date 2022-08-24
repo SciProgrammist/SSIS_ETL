a01d880acce81e67feea7d3fa2c932ed921baa69
SciProgrammist <RP191495@alumno.udb.edu.sv>
2022-08-24 08:52:32 -0600

    ETL_Procedures

---
+++ Ejercicio2/BDQuery_Control_Ventas.sql
@@ -0,0 +1,16 @@
+
+
+--Ejercicio 02 DMD05L Importar datos de un archivo .xlsx a SQL Server.
+
+create database Control_Ventas
+go
+
+use Control_Ventas
+
+--Verificacion de datos extraidos del archivo de Excel a la tabla Query de la base de datos Control_Ventas
+SELECT*FROM Query
+
+--Daniel Alexander Reyes Pineda RP191495.
+
+--Verificacion de ejercicio2 con asistente de VS.
+SELECT*FROM Ventas$
